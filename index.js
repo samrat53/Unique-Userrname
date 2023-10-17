70bc067314e06406e764309f892dab3ecb56f8d7
samrat53 <samrat05hccs@gmail.com>
2023-10-17 19:34:31 +0530

    generated username and passed to UI

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
-const app=express();
-const PORT= 5173;
+import genUsername from "unique-username-generator";
+const app = express();
+const PORT = 5173;
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs");
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.post("/submit",(req,res)=>{
-    const email=req.body["inputEmail"];
-    const digits=req.body["inputNumOfDigits"];
-    const separator=req.body["inputSeparator"];
-    console.log(`email: ${email} digits: ${digits} separator: ${separator}`);
-})
+app.post("/submit", (req, res) => {
+  const email = req.body["inputEmail"];
+  const digits = req.body["inputNumOfDigits"]; //value parsed is passed as integer
+  const castedDigit = +digits; //typecasting
+  const username = genUsername.generateFromEmail(email, castedDigit);
+  console.log(`email: ${email} digits: ${digits}`);
+  console.log(`username: ${username}`);
+  res.render("index.ejs", { username: username });
+});
 
-app.listen(PORT,()=>console.log(`Port ${PORT} active`));
+app.listen(PORT, () => console.log(`Port ${PORT} active`));

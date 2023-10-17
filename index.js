a55e6ff409baab639860168639b2e055972c985e
samrat53 <samrat05hccs@gmail.com>
2023-10-17 18:59:27 +0530

    retrive data from the UI

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ import bodyParser from "body-parser";
 const app=express();
 const PORT= 5173;
 app.use(express.static("public"));
-app.listen(PORT,()=>console.log(`Port ${PORT} active`));
-
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 });
+
+app.post("/submit",(req,res)=>{
+    const email=req.body["inputEmail"];
+    const digits=req.body["inputNumOfDigits"];
+    const separator=req.body["inputSeparator"];
+    console.log(`email: ${email} digits: ${digits} separator: ${separator}`);
+})
+
+app.listen(PORT,()=>console.log(`Port ${PORT} active`));

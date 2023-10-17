6bc0a4f939ec9f1e2a77d8a33e8924e3a465a6b4
samrat53 <samrat05hccs@gmail.com>
2023-10-17 13:33:20 +0530

    initialize static files, add UI elements & manage file structure

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 const app=express();
 const PORT= 5173;
+app.use(express.static("public"));
 app.listen(PORT,()=>console.log(`Port ${PORT} active`));
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
-})
+});
